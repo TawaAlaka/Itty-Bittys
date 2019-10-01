3152544b138a17507bb524b377618cadeff1b6fc
Chase Brewer <chasepbrewer@gmail.com>
2019-09-30 20:20:29 -0400

    Add comments to project

--- server/core/views.py
+++ server/core/views.py
@@ -6,43 +6,69 @@ from . import models, serializers
 
 
 class UserViewSet(ModelViewSet):
+    """API Views related with user objects."""
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
 
 
-
 class ConditionViewSet(ModelViewSet):
+    """API Views related with long term conditions."""
     queryset = models.Condition.objects.all()
     serializer_class = serializers.ConditionSerializer
 
 
 class AilmentViewSet(ModelViewSet):
+    """API Views related to short term ailments."""
     queryset = models.Ailment.objects.all()
     serializer_class = serializers.AilmentSerializer
 
 
 class FoodViewSet(ModelViewSet):
+    """API Views related with food objects."""
     queryset = models.Food.objects.all()
     serializer_class = serializers.FoodSerializer
 
 
 class MealViewSet(ModelViewSet):
+    """API Views related with meal objects."""
     queryset = models.Meal.objects.all()
     serializer_class = serializers.MealSerializer
 
     def get_queryset(self):
+        """Gets the queryset for the views.
+
+        Returns:
+            Queryset of all meals filtered to the current user.
+        """
         return self.queryset.filter(log__user=self.request.user)
 
 
 class LogViewSet(ModelViewSet):
+    """API Views related with daily logs."""
     queryset = models.Log.objects.all()
     serializer_class = serializers.LogSerializer
 
     def get_queryset(self):
+        """Gets the queryset for the views.
+
+        Returns:
+            Queryset of all logs filtered to the current user.
+        """
         return self.queryset.filter(user=self.request.user)
 
     @action(['GET', 'POST'], True, url_name='meal-list')
     def meals(self, request):
+        """View that lists and adds meals for a log.
+
+        POST requests will add a meal, and GET requests will list the
+        meals for that log.
+
+        Args:
+            request: Request from the user.
+
+        Returns:
+            List of meals or the created meal.
+        """
         log = self.get_object()
         # Add a meal if it's a POST request.
         if request.method == 'POST':
